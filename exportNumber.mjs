418938638e26c4951d5ed27c8c8d3ddbe4c5801c
GattuSaipranay <saipranaysrinivas@gmail.com>
2024-03-02 11:14:20 +0000

    Import and Export a Number using ES6 Module Syntax

---
+++ exportNumber.mjs
@@ -0,0 +1,5 @@
+// Define the number to export
+const numberToExport = 25;
+
+// Export the number using default export syntax
+export default numberToExport;
---
+++ importNumber.mjs
@@ -0,0 +1,5 @@
+// Import the default export
+import numberToImport from './exportNumber.mjs';
+
+// Test the imported number
+console.log("Imported number:", numberToImport);
